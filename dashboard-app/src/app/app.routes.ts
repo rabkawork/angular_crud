60980dac6a60951444b50cfb09a1669eb0abaa6a
Ahadian Akbar <rabkawork@gmail.com>
2024-12-24 01:38:28 +0700

    remove useless code

--- dashboard-app/src/app/app.routes.ts
+++ dashboard-app/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { ContentComponent } from './pages/content/content.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  // { path: '', redirectTo: 'content', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: 'content', pathMatch: 'full' }, // Default route
   { path: 'content', component: ContentComponent },
 ];
